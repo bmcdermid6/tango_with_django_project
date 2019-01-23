4951b6a54242a8fbe42f129be1344fccc1b6976a
Beth McDermid <2299260m@student.gla.ac.uk>
2019-01-23 21:26:38 +0000

    trying to make chap 3 automated tests run

--- rango/views.py
+++ rango/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    #construct a dictionary to pass to the template engine as its contet
-    #note the key boldmessage is the same as {{ boldmessage }} in the template
-    context_dict = {'boldmessage': "Crunchy, ceamy, cookie, candy, cupcake!"}
-    
-    #return a rendered response to send to the client
-    return render(request, 'rango/index.html', context=context_dict)
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 
 def about(request):
-    context_dict = {'boldmessage':"Picture of a kitten and a puppy"}
-    return render(request, 'rango/about.html', context=context_dict)
+    return HttpResponse("Rango says here is the about page. <br/> <a href = '/rango/'>Index</a>")
