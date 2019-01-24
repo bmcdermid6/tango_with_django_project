9609ff56b4a0731c2b78d717f0b4eb01cf9a0929
Sophie Hope <bethmcdermid522@gmail.com>
2019-01-24 17:38:19 +0000

    chapter 5

--- populate_rango.py
+++ populate_rango.py
@@ -29,12 +29,12 @@ def populate():
         {"title":"Flask",
          "url":"http://flask.pocoo.org"}]
     
-    cats = {"Python":{"pages":python_pages, "views"=128, "likes":64},
+    cats = {"Python":{"pages":python_pages, "views":128, "likes":64},
             "Django":{"pages":django_pages, "views":64, "likes":32},
             "Other Frameworks":{"pages":other_pages, "views":32, "likes":16}}
     
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
@@ -44,6 +44,8 @@ def populate():
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
+            
+            
 def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
@@ -53,8 +55,8 @@ def add_page(cat, title, url, views=0):
 
 def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
-    c.name=name
     c.likes=likes
+    c.views=views
     c.save()
     return c
 
--- rango/views.py
+++ rango/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+    #construct a dictionary to pass to the template engine as its contet
+    #note the key boldmessage is the same as {{ boldmessage }} in the template
+    context_dict = {'boldmessage': "Crunchy, ceamy, cookie, candy, cupcake!"}
+    
+    #return a rendered response to send to the client
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href = '/rango/'>Index</a>")
+    context_dict = {'boldmessage':""}
+    return render(request, 'rango/about.html', context=context_dict)
+
 
 
